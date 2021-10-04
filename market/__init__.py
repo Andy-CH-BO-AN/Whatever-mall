e201d68c41d527fe0df076a73f4852396146fc9e
Andy <andypig9@gmail.com>
2021-10-04 22:01:04 +0800

    Create credential

--- market/__init__.py
+++ market/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = '07a4f262791313bc6a0177bd'
 db = SQLAlchemy(app)
 
 from market import routes
