33b567bf88f1da0a652f77d5a56e67b086a6cb27
Andy-CH-BO-AN <58759761+Andy-CH-BO-AN@users.noreply.github.com>
2023-10-22 20:17:36 +0800

    Update __init__.py
    Delete sensitive information

--- market/__init__.py
+++ market/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-app.config['SECRET_KEY'] = '07a4f262791313bc6a0177bd'
+app.config['SECRET_KEY'] = ''
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
